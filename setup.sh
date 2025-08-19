c538ef5b68a8c13728d028b694d7a4b0adf74701
Maxi <120334272+Maxi2710@users.noreply.github.com>
2025-08-19 16:26:42 +0200

    19.08.2025

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 sudo apt install python3
 sudo apt install python3-yaml
+mkdir /usr/local/bin/nut-wakeonlan/
+mkdir /etc/nut/wakeonlan/
+mv main.py /usr/local/bin/nut-wakeonlan/main.py
+mv nut-wakeonlan.service /etc/systemd/system/nut-wakeonlan.service
